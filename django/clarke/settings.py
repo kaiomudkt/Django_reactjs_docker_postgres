ea8de2c3ed041c89c8bef937c05801b88097c03e
kaio <kaiomudkt@gmail.com>
2024-08-02 22:29:34 -0400

    liberando localhost

--- django/clarke/settings.py
+++ django/clarke/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -25,8 +24,7 @@ SECRET_KEY = 'django-insecure-py9fs#vr8+*l8=gvv)mh9v@f)8e7&+hvosl$5pcnvhtyb)r^qd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
 
 # Application definition
 
