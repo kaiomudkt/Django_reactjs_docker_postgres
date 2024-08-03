44c7777a0e0739514b0c2f80f9d631ee7ab5fbdd
kaio <kaiomudkt@gmail.com>
2024-08-03 13:10:13 -0400

    apagando comentario

--- django/clarke/settings.py
+++ django/clarke/settings.py
@@ -75,17 +75,6 @@ WSGI_APPLICATION = 'clarke.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': env('POSTGRES_DB'),
-#         'USER': env('POSTGRES_USER'),
-#         'PASSWORD': env('POSTGRES_PASSWORD'),
-#         'HOST': env('POSTGRES_HOST'),
-#         'PORT': env('POSTGRES_PORT'),
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
